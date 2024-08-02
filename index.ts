ea6190115e7e81f0ef9e582ddd8b1cfe176ca43a
user <user>
2024-08-02 14:20:02 -0700

    retrieve weather

--- index.ts
+++ index.ts
@@ -1,7 +1,38 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
+
+const getWeather = async ({ latitude, longitude }: {
+  latitude: string;
+  longitude: string;
+}) => {
+  const result = await fetch(
+    `https://api.weather.gov/points/${latitude},${longitude}`
+  );
+  const data = await result.json();
+
+  const forecastHourlyUrl = data.properties.forecastHourly;
+  const forecastHourlyResult = await fetch(forecastHourlyUrl);
+
+  const forecastHourlyData = await forecastHourlyResult.json();
+
+  return forecastHourlyData.properties.periods;
+}
 
 new Elysia()
-  .get("/", () => "hello world")
+  .get(
+    "/weather",
+    async ({ query }) => {
+      const { latitude, longitude } = query;
+      const weather = await getWeather({ latitude, longitude });
+
+      return weather;
+    },
+    {
+      query: t.Object({
+        latitude: t.String(),
+        longitude: t.String(),
+      }),
+    }
+  )
   .listen(3000, () => {
     console.log("Server is running on port 3000");
   });
