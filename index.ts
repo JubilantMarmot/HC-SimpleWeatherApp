2c1631b65291f3cb553d20571336665c0b924972
user <user>
2024-08-02 14:04:44 -0700

    more stuff

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import Elysia from "elysia";
+
+new Elysia()
+  .get("/", () => "hello world")
+  .listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
